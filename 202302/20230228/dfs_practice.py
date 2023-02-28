eba409fc1bd12c07a45b87baa1fd4f7e8bf14323
jungdawoon <tyranno2020@gmail.com>
2023-02-28 16:36:01 +0900

    pycharm study dfs

--- 202302/20230228/dfs_practice.py
+++ 202302/20230228/dfs_practice.py
@@ -1,62 +1,44 @@
-# V, E = 7, 8
-# arr = list(map(int, "1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7".split()))
-#
-# print(arr)
-#
-# # 인접 리스트
-# adjList = [[] for _ in range(V + 1)]
-#
-# for i in range(E):
-#     n1, n2 = arr[i * 2], arr[i * 2 + 1]
-#     adjList[n1].append(n2)
-#     adjList[n2].append(n1)
-#
-#
-# adjList[0].append(1)
-# print(adjList)
-
-
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
 n, e = map(int, input().split())
 
-adjList = [[] for _ in range(n + 1)]
-adjCheck = [False for _ in range(n + 1)]
-adjCheck[0] = True
+A = [[] for i in range(n + 1)]
+R = [False for i in range(n + 1)]
+R[0] = True
 
-for _ in range(e):
-    z, x = map(int, input().split())
-    adjList[z].append(x)
-
-
-def recursion_def(param):
-    li = adjList[param]
-    f_count = 0
-    for k in li:
-        if not adjCheck[k]:
-            adjCheck[k] = True
-            recursion_def(k)
-            f_count += 1
-
-    return f_count
+for i in range(e):
+    a, b = map(int, input().split())
+    A[a].append(b)
+    A[b].append(a)
 
 
-count = 0
-for i in range(n+1):
-    if not adjCheck[i]:
-        c = recursion_def(i)
-        if c > 0:
-            adjCheck[i] = True
+def check_recursion(param):
+    count = 0
+    result = False
+    for i in A[param]:
+        if not R[i]:
+            R[i] = True
             count += 1
+            check_recursion(i)
+    if count > 0:
+        result = True
+        R[param] = True
+    return result
 
-print(adjCheck)
 
-for i in range(n+1):
-    if not adjCheck[i]:
-        adjCheck[i] = True
-        count += 1
+rcount = 0
+#check recursion
+for i in range(n):
+    if not R[i]:
+        if check_recursion(i):
+            rcount += 1
 
-print(count)
+#not recursion 체크
+for i in range(n + 1):
+    if not R[i]:
+        R[i] = True
+        rcount += 1
 
+print(rcount)

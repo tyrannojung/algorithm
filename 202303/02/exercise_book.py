387e3ca68cbdeefc0f0ddd43bfe14eb878a63dd6
DESKTOP-929CA5E\jds00 <tyranno2020@gmail.com>
2023-03-02 10:59:48 +0900

    home commit

---
+++ 202303/02/exercise_book.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n, e = map(int, input().split())
+
+initialize_dps = [[] for _ in range(e)]
+initialize_check = [False for _ in range(e)]
+
+
+def initialize_dps_function():
+    global initialize_check
+    initialize_check = [False for _ in range(e)]
+
+
+for i in range(e):
+    k, x = map(int, input().split())
+    initialize_dps[k].append(x)
+    initialize_dps[x].append(k)
+
+print(initialize_dps)
+
+def DFS(param):
+    for m in range(len(param)):
+        if not initialize_check[m]:
+            initialize_check[m] = True
+            DFS(initialize_dps[m])
+
+
+for i in range(n):
+    initialize_dps_function()
+    DFS(initialize_dps[i])
+    print(initialize_check)
+

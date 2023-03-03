48c559c6d4d31f0be59610f2f9f5c20bcd8fbc0f
DESKTOP-929CA5E\jds00 <tyranno2020@gmail.com>
2023-03-03 18:17:32 +0900

    home fail commit

---
+++ 202303/03/exercise.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n, e = map(int, input().split())
+
+initialize_dps = [[] for _ in range(n)]
+initialize_check = [False for _ in range(n)]
+
+
+def initialize_dps_function():
+    global initialize_check
+    initialize_check = [False for _ in range(n)]
+
+
+for i in range(e):
+    k, x = map(int, input().split())
+    initialize_dps[k].append(x)
+    initialize_dps[x].append(k)
+
+print(initialize_dps)
+
+
+def DFS(param):
+    for m in range(len(param)):
+        print("param =====" + str(initialize_dps[m]))
+        print("init ====" + str(initialize_dps[m]))
+        print(initialize_check)
+        if not initialize_check[m]:
+            initialize_check[m] = True
+            DFS(initialize_dps[m])
+        else:
+            continue
+
+
+for i in range(n):
+    initialize_check[i] = True
+    DFS(initialize_dps[i])
+

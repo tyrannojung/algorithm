a41d72d50f774f19e585be600de355a1fd7daf8f
jungdawoon <tyranno2020@gmail.com>
2023-03-21 12:49:41 +0900

    update

--- 202303/20/practice.py
+++ 202303/20/practice.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+# https://www.acmicpc.net/board/view/77253 참고해서 풀자
 n, e = map(int, input().split())
 
 c_puzzle = deque()
@@ -13,81 +13,68 @@ for i in range(n):
 c_puzzle.append(puzzle)
 ch_puzzle.append([0, 0, 0])
 
-# a_puzzle = deque()
-# for i in range(len(c_puzzle)):
-#     k = list()
-#     for z in c_puzzle[i]:
-#         if z == 1:
-#             k.append(True)
-#         else:
-#             k.append(False)
-#     a_puzzle.append(k)
-# print(a_puzzle)
+while True:
+    print(c_puzzle)
+    if ch_puzzle:
+        bfs_l = c_puzzle.popleft()
+        bfs_p = ch_puzzle.popleft()
+
+        row_num = bfs_p[0]
+        column_num = bfs_p[1]
+
+        #east west south north
+        east = column_num + 1
+        north = row_num - 1
+        west = column_num - 1
+        south = row_num + 1
+
+        if row_num == n-1 and column_num == e-1:
+            t_puzzle.append(bfs_p[2] + 1)
+            continue
+
+        if east < e and bfs_l[row_num][east] == 1:
+            east_bfs_l = [arr[:] for arr in bfs_l]
+            east_bfs_p = bfs_p[:]
+            east_bfs_l[row_num][column_num] = 0
+            east_bfs_p[0] = row_num
+            east_bfs_p[1] = east
+            east_bfs_p[2] = east_bfs_p[2] + 1
+            c_puzzle.append(east_bfs_l)
+            ch_puzzle.append(east_bfs_p)
+
+        if north >= 0 and bfs_l[north][column_num] == 1:
+            north_bfs_l = [arr[:] for arr in bfs_l]
+            north_bfs_p = bfs_p[:]
+            north_bfs_l[row_num][column_num] = 0
+            north_bfs_p[0] = north
+            north_bfs_p[1] = column_num
+            north_bfs_p[2] = north_bfs_p[2] + 1
+            c_puzzle.append(north_bfs_l)
+            ch_puzzle.append(north_bfs_p)
+
+        if west >= 0 and bfs_l[row_num][west] == 1:
+            west_bfs_l = [arr[:] for arr in bfs_l]
+            west_bfs_p = bfs_p[:]
+            west_bfs_l[row_num][column_num] = 0
+            west_bfs_p[0] = row_num
+            west_bfs_p[1] = west
+            west_bfs_p[2] = west_bfs_p[2] + 1
+            c_puzzle.append(west_bfs_l)
+            ch_puzzle.append(west_bfs_p)
+
+        if south < n and bfs_l[south][column_num] == 1:
+            south_bfs_l = [arr[:] for arr in bfs_l]
+            south_bfs_p = bfs_p[:]
+            south_bfs_l[row_num][column_num] = 0
+            south_bfs_p[0] = south
+            south_bfs_p[1] = column_num
+            south_bfs_p[2] = south_bfs_p[2] + 1
+            c_puzzle.append(south_bfs_l)
+            ch_puzzle.append(south_bfs_p)
+
+    else:
+        break
 
 
-
-
-# while True:
-if ch_puzzle:
-    bfs_l = c_puzzle.popleft()
-    bfs_p = ch_puzzle.popleft()
-
-    row_num = bfs_p[0]
-    column_num = bfs_p[1]
-
-    #east west south north
-    east = column_num + 1
-    north = row_num - 1
-    west = row_num - 1
-    south = column_num + 1
-
-    if east < e and bfs_l[row_num][east] == 1:
-        east_bfs_l = bfs_l.copy()
-        east_bfs_p = bfs_p.copy()
-        east_bfs_l[row_num][column_num] = 0
-        east_bfs_p[0] = row_num
-        east_bfs_p[1] = east
-        east_bfs_p[2] = east_bfs_p[2] + 1
-        c_puzzle.append(east_bfs_l)
-        ch_puzzle.append(east_bfs_p)
-
-    if north >= 0 and bfs_l[north][column_num] == 1:
-        north_bfs_l = bfs_l.copy()
-        north_bfs_p = bfs_p.copy()
-        north_bfs_l[row_num][column_num] = 0
-        north_bfs_p[0] = north
-        north_bfs_p[1] = column_num
-        north_bfs_p[2] = north_bfs_p[2] + 1
-        c_puzzle.append(north_bfs_l)
-        ch_puzzle.append(north_bfs_p)
-
-    if west >= 0 and bfs_l[row_num][west] == 1:
-        west_bfs_l = bfs_l.copy()
-        west_bfs_p = bfs_p.copy()
-        west_bfs_l[row_num][column_num] = 0
-        west_bfs_p[0] = row_num
-        west_bfs_p[1] = west
-        west_bfs_p[2] = west_bfs_p[2] + 1
-        c_puzzle.append(west_bfs_l)
-        ch_puzzle.append(west_bfs_p)
-
-    if south < n and bfs_l[south][column_num] == 1:
-        south_bfs_l = bfs_l.copy()
-        south_bfs_p = bfs_p.copy()
-        south_bfs_l[row_num][column_num] = 0
-        south_bfs_p[0] = south
-        south_bfs_p[1] = column_num
-        south_bfs_p[2] = south_bfs_p[2] + 1
-        c_puzzle.append(south_bfs_l)
-        ch_puzzle.append(south_bfs_p)
-
-else:
-    #break
-    print("hi")
-
-
-print(c_puzzle)
-print(ch_puzzle)
-
-
+print(min(t_puzzle))
 

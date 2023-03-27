17b5ebfd54ebf8039246c3ba1ed6ab8b864e075a
jungdawoon <tyranno2020@gmail.com>
2023-03-27 13:36:12 +0900

    Success

--- 202303/27/practice.py
+++ 202303/27/practice.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 print = sys.stdout.write
 
@@ -8,27 +9,40 @@ f = int(input())
 k_list = list(map(int, input().split()))
 c_list.sort()
 
+c_start = 0
+c_end = len(c_list) - 1
+c_mid = 0
+
 
 def find_num(param):
-    f_list = c_list.copy()
-    while True:
-        c_mid_len = len(f_list)
-        c_mid = c_mid_len // 2
+    global c_start, c_end, c_mid
+    c_start = 0
+    c_end = len(c_list) - 1
+    c_mid = 0
 
-        if c_mid_len == 0:
-            print("0" + '\n')
-            break
-        elif f_list[c_mid] == param:
+    while True:
+        c_sum = c_start + c_end
+        if c_sum % 2 == 0:
+            c_mid = c_sum // 2
+        else:
+            c_mid = (c_sum // 2) + 1
+
+        if c_list[c_mid] == param:
             print("1" + '\n')
             break
-        elif c_mid_len == 1:
-            print("0" + '\n')
+
+        if c_end - c_start == 1 or c_end == c_start:
+            if c_list[c_start] == param or c_list[c_end] == param:
+                print("1" + '\n')
+            else:
+                print("0" + '\n')
             break
 
-        if f_list[c_mid] > param:
-            f_list = f_list[:c_mid]
+        if c_list[c_mid] > param:
+            c_end = c_mid - 1
         else:
-            f_list = f_list[c_mid+1:]
+            c_start = c_mid + 1
+
 
 for i in k_list:
     find_num(i)

0950de6c30558977aede3fddaf01400f9266fa5a
jungdawoon <tyranno2020@gmail.com>
2023-03-30 10:42:09 +0900

    update

--- 202303/27/binarySearch_practice.py
+++ 202303/27/binarySearch_practice.py
@@ -3,74 +3,36 @@
 n, e = map(int, input().split())
 n_list = list(map(int, input().split()))
 n_list.sort()
+min_num = max(n_list)
+max_num = sum(n_list)
 
+for z in range(100):
+    check_sum = max_num + min_num
 
-k = 0
-n_list_len = len(n_list)
-l_sum = n_list[n_list_len - 1]
+    print(max_num, min_num)
+    if check_sum % 2 != 0:
+        check_sum + 1
 
-while True:
-    check = False
-    i = 0
-    sum_check = 0
-    for z in range(n_list_len):
-        sum_check += n_list[z]
-        i += 1
-        if sum_check > l_sum:
-            i -= 1
-            break
+    k = check_sum // 2
+    sum = 0
+    count = 1
+    for i in range(len(n_list)):
+        sum += n_list[i]
+        if sum > k:
+            sum = 0
+            sum += n_list[i]
+            count += 1
+    print("count == " + str(count))
 
-    sum_check = 0
-    for y in range(i, n_list_len - k):
-        sum_check += n_list[y]
-        i += 1
-        if sum_check > l_sum:
-            i -= 1
-            break
+    if count < e:
+        max_num = k - 1
+    elif count > e:
+        min_num = k + 1
+    elif count == e:
+        max_num = k
 
-    if i+k+1 == n_list_len:
-        check = True
-        print(l_sum)
+    count = 1
+    if min_num == max_num:
+        print(max_num)
         break
-    else:
-        k += 1
-        l_sum += n_list[n_list_len-1-k]
-
-
-
-
-
-# 7 7
-# 5 9 6 8 7 7 5
-#
-# 답: 9
-#
-# 0번합친다. max값
-#
-# 8 7
-# 3 3 10 10 3 2 6 2
-#
-# 답: 10
-#
-# 1번합치고
-#
-#
-# 9 3
-# 1 2 3 4 5 6 7 8 9
-#
-# 3번합치고
-#
-# 7 6
-# 100 100 101 300 400 400 500
-# 답: 500
-#
-# 4 2
-# 1 1 1 1
-# 답: 2
-#
-# 7 7
-# 1 5 9 9 9 2 9
-#
-# 답: 9
-
 

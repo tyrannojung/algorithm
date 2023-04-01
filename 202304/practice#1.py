914ba7094d562b50eb3beb64e23200cb5f13365d
jungdawoon <tyranno2020@gmail.com>
2023-04-01 10:57:07 +0900

    success

--- 202304/practice#1.py
+++ 202304/practice#1.py
@@ -7,8 +7,6 @@ input = sys.stdin.readline
 n, e = map(int, input().split())
 n_list = deque([0] * n)
 
-k_list = deque(list(map(int, str(e))))
-
 for i in range(n):
     n_list[i] = int(input())
 
@@ -18,14 +16,9 @@ for _ in range(n):
     if e - k < 0:
         continue
     else:
-        z = k_list.popleft()
-        if str(k).rstrip("0") == "5":
-            e -= k
-            count += 1
-            k_list.appendleft(z - 5)
-        else:
-            e -= k * z
-            count += z
+        z = e//k
+        e -= k * z
+        count += z
 
 print(count)
 

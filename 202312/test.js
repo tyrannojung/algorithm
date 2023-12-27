dbe4c3888ca982d05ef08879593669a3c3f92b89
tyrannojung <tyranno2020@gmail.com>
2023-12-27 23:04:07 +0900

    Coding Test I/O Practice

--- 202312/test.js
+++ 202312/test.js
@@ -1,4 +1,50 @@
-let fs = require('fs');
-let input = fs.readFileSync('example.txt') // 제출할때는 /dev/stdin로 변경
-.toString().split(' ');
-console.log(input);
+// let fs = require('fs');
+// let input = fs.readFileSync('example.txt') // 제출할때는 /dev/stdin로 변경
+// .toString().split(' ');
+// let sum = parseInt(input[0]) + parseInt(input[1])
+// console.log(sum);
+
+// const fs = require('fs');
+// const input = fs.readFileSync("example.txt").toString().trim().split("\n");
+// let sum = parseInt(input[0]) + parseInt(input[1])
+// console.log(sum)
+
+/* 10950 */
+// const fs = require('fs');
+// const input = fs.readFileSync('example.txt').toString().split('\n');
+// const num = parseInt(input[0]);
+// input.shift();
+
+// for(let i = 0; i < num; i++ ){
+//     let input_value = input[i].split(' ').map(Number);
+//     let sum = input_value[0] + input_value[1];
+//     console.log(sum)
+// }
+
+/* 10951 */
+// const fs = require('fs');
+// const input = fs.readFileSync('example.txt').toString().trim().split('\n');
+
+// for(let i = 0; i < input.length; i++ ){
+//     let input_value = input[i].split(' ').map(Number);
+//     let sum = input_value[0] + input_value[1];
+//     console.log(sum)
+// }
+
+/* 10952 */
+// const fs = require('fs');
+// const input = fs.readFileSync('example.txt').toString().trim().split('\n');
+
+// for(let i = 0; i < input.length; i++ ){
+//     let input_value = input[i].split(' ').map(Number);
+//     if(!input_value[0] || !input_value[1]){
+//         continue
+//     }
+//     let sum = input_value[0] + input_value[1];
+//     console.log(sum)
+// }
+
+
+// let sum = parseInt(inputArr[0]) + parseInt(inputArr[1])
+
+// console.log(sum)

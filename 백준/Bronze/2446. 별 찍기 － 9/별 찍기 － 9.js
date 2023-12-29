ae28b7eaeb9f038c7e6afac463bb3e8343239bfd
Dawoon Jung <tyranno2020@gmail.com>
2023-12-30 01:02:52 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 116 ms, Memory: 9424 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+let result = Array.from({length : input}, (_ , i) => ' '.repeat(input - i -1) + '*'.repeat(i * 2 + 1))
+let result_reverse = [...result].reverse()
+result.shift()
+console.log(result_reverse.join('\n'))
+console.log(result.join('\n'))
